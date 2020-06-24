d785997a3cfec47b18b1c37e1f4ae62789267bf5
Falco Femoralis <araraunapappagallo@gmail.com>
2020-06-24 15:20:08 +0300

    Добавил недостающие скрипты

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour
+{
+    Text text;
+
+    private void Start()
+    {
+        text = GetComponent<Text>();
+        text.enabled = false;
+    }
+
+    void Update()
+    {
+        if (Checker.upCount == 0)
+        {
+            text.text = "Up Win!";
+            text.enabled = true;
+            //Debug.Log("Up Win!");
+           // Debug.Log("upCount=" + Checker.upCount + " and downCount=" + Checker.downCount);
+
+        }
+
+        if (Checker.downCount == 0)
+        {
+            text.text = "Down Win!";
+            text.enabled = true;
+           // Debug.Log("down Win!");
+           // Debug.Log("upCount=" + Checker.upCount + " and downCount=" + Checker.downCount);
+        }
+    }
+}
