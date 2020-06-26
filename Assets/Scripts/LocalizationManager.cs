5e17266ec1d1645e57153881703462b89cfbce75
Illia <derladoshome@gmail.com>
2020-06-26 19:26:00 +0300

    add comments and add small changes in LocalizationManager script

--- Assets/Scripts/LocalizationManager.cs
+++ Assets/Scripts/LocalizationManager.cs
@@ -5,13 +5,24 @@ using System.Xml.Linq;
 using UnityEngine.SceneManagement;
 using BaseStructures;
 
+/* Класс для работы с локализацией текста
+ * Singleton
+ */
 public class LocalizationManager
 {
     static List<Pair<Text, string>> texts = new List<Pair<Text, string>>();
     static LocalizationManager instance = null;
     
     XElement data; // Данные XML файла
-    string curLanguage = "UA"; // Текущий язык
+
+    // Набор языков
+    enum language : byte
+    {
+        RU,
+        EU,
+        UA
+    }
+    language curLanguage = language.UA; // Текущий язык
 
     private LocalizationManager()
     { }
@@ -22,24 +33,27 @@ public class LocalizationManager
         return instance;
     }
 
+    // Добавление текста в список текста который необходимо локализовать
     public void add(Pair<Text, string> text)
     {
         texts.Add(text);
     }
 
+    // Инициализация (Загрузка XML файла и установка языка)
     public void Init()
     {
         loadXML();
         setLanguage();
     }
 
+    // Загрузка данных из XML файла
     private void loadXML()
     {
-        Debug.Log(curLanguage + "/" + SceneManager.GetActiveScene().name);
-        TextAsset textAsset = (TextAsset) Resources.Load(curLanguage + "/" + SceneManager.GetActiveScene().name);
+        TextAsset textAsset = (TextAsset) Resources.Load(curLanguage.ToString() + "/" + SceneManager.GetActiveScene().name);
         data = XDocument.Parse(textAsset.text).Element("localization");
     }
 
+    // Установка языка
     private void setLanguage()
     {
         for (int i = 0; i < texts.Count; ++i)
