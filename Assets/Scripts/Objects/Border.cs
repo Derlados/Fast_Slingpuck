5928a5f849d9137d7f6ad9f50b1dcaf098a75dfc
Illia <derladoshome@gmail.com>
2020-06-27 19:41:14 +0300

    return border.cs

---
+++ Assets/Scripts/Objects/Border.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Border : MonoBehaviour
+{
+    void Start()
+    {
+        ScreenOptimization.setColider(gameObject, gameObject.GetComponent<BoxCollider2D>());
+    }
+
+
+}
