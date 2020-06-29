324f9b986660151f5bb4eb3916f881e4778ef0ad
Illia <derladoshome@gmail.com>
2020-06-29 19:07:46 +0300

    improved Camera Manager, create GameMenu scene v.0

--- Assets/Scripts/UI/CameraManager.cs
+++ Assets/Scripts/UI/CameraManager.cs
@@ -4,36 +4,72 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    public Canvas can1, can2;
-    public Camera camera;
-    public float speedSize, speedMove;
+    public Canvas Menu, Planets; // Полотоно Меню и Планет соответсвенно
+    public Camera thisCamera; // Камера на которую вешается скрипт
+    private Vector2 startPos, target; // StartPos - начальная позиция камеры, target - позиция планеты к которой необходимо приблизить камеру
+    public float dirrection, speedSize, speedMove; // Скорость приближения камеры. speedSize - скорость изменения размера видимой области, speedMove - скорость перемещения камеры
+    private GameObject planetLevels;
 
-    Status cameraStatus = Status.free;
+
+    Status cameraStatus = Status.freeOnMenu;
 
     enum Status
     {
-        free,
-        zoom,
+        freeOnMenu, // Камера свободна и находится в меню
+        freeOnPlanet, // Камера свободна и находится на планете
+        zoom, // Камера приближается
     }
 
     private void Start()
     {
-        can2.GetComponent<RectTransform>().sizeDelta = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height)) * 2;
+        startPos = thisCamera.transform.position;
+        // Оптимизация второго поля под разные екраны так как поле Планет не закреплено за камерой
+        Planets.GetComponent<RectTransform>().sizeDelta = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height)) * 2;
     }
 
     private void Update()
     {
         if (cameraStatus == Status.zoom)
         {
-            Vector2 temp = Vector2.MoveTowards(camera.transform.position, gameObject.transform.position, Time.deltaTime * speedMove);
+            Vector2 temp = Vector2.MoveTowards(thisCamera.transform.position, target, Time.deltaTime * speedMove);
 
-            if (temp == (Vector2)gameObject.transform.position)
-                cameraStatus = Status.free;
+            if ((Vector2)thisCamera.transform.position == target)
+            {
+                cameraStatus = (Vector2)thisCamera.transform.position == startPos ? Status.freeOnMenu : Status.freeOnPlanet;
+                if (cameraStatus == Status.freeOnPlanet)
+                    planetLevels.SetActive(true);
+            }
 
-            camera.transform.position = new Vector3(temp.x, temp.y, camera.transform.position.z);
-            camera.orthographicSize -= Time.deltaTime * speedSize;
+            thisCamera.transform.position = new Vector3(temp.x, temp.y, thisCamera.transform.position.z);
+            thisCamera.orthographicSize -= Time.deltaTime * speedSize * dirrection;
         }
 
+    }
+
+    // Приближение к планете
+    public void zoomPlanet(GameObject planet)
+    {
+        if (cameraStatus != Status.freeOnPlanet)
+        {         
+            target = planet.transform.position;
+            dirrection = 1;
+            cameraStatus = Status.zoom;
+        }
+    }
+
+    public void setPlanetsLevels(GameObject planetLevels)
+    {
+        this.planetLevels = planetLevels;
+    }
+
+    // Возврат к стартовому меню
+
+    public void backToStart()
+    {
+        dirrection = -1;
+        target = startPos;
+        planetLevels.SetActive(false);
+        cameraStatus = Status.zoom;    
     }
 }
 
