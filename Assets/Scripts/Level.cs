30909e77f7513b3f76548b1eb83445a7c2190309
Illia <derladoshome@gmail.com>
2020-06-30 16:53:48 +0300

    add PlayerData.cs and add visual LVL progress

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+// Этот класс должен храниь все данные уровня (сложность ИИ, тип поля, режим и т.д.)
+public class Level : MonoBehaviour
+{
+    public int NumberPlanet, NumberLvl;
+
+    public void OnEnable()
+    {
+        if (!PlayerData.getInstance().progress[NumberPlanet][NumberLvl])
+        {
+            Color32 thisColor = gameObject.GetComponent<Image>().color; 
+            gameObject.GetComponent<Image>().color = new Color32(thisColor.r, thisColor.g, thisColor.b, 170);
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Этот класс должен будет хранить все данные пользователя
+[System.Serializable]
+public class PlayerData : MonoBehaviour
+{
+    static PlayerData instance;
+
+    public int money;
+    public List<List<bool>> progress = new List<List<bool>>();
+
+    public void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        progress.Add(new List<bool>());
+        progress[0].Add(true);
+        progress[0].Add(true);
+        progress[0].Add(false);
+        progress[0].Add(false);
+    }
+
+    private PlayerData() { }
+
+    public static PlayerData getInstance()
+    {
+        return instance;
+    }
+
+}
