3b9c4c06f0bf079a20343b20b297db025e82ec46
Falco Femoralis <araraunapappagallo@gmail.com>
2020-07-20 14:29:15 +0300

    Implements Google Play Games API

---
+++ Assets/Scripts/UI/GPGSAchievements.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPGSAchievements : MonoBehaviour
+{
+    public void OpenAchievementPanel()
+    {
+        Social.ShowAchievementsUI();
+    }
+}
