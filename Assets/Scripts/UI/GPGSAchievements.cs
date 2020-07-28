c5ba1caa41bac10247959d97ebf17ed87e2c0cbe
Falco Femoralis <araraunapappagallo@gmail.com>
2020-07-28 23:41:58 +0300

    Added GPGSAchievements

--- Assets/Scripts/UI/GPGSAchievements.cs
+++ Assets/Scripts/UI/GPGSAchievements.cs
@@ -1,11 +1,26 @@
-﻿using System.Collections;
+﻿using GooglePlayGames;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GPGSAchievements : MonoBehaviour
 {
     public void OpenAchievementPanel()
     {
         Social.ShowAchievementsUI();
+    }
+
+    public void UpdateIncremental()
+    {
+        //увеличение ачивки на 1
+        PlayGamesPlatform.Instance.IncrementAchievement(GPGSIds.achievement_goals, 1, null);
+    }
+
+  
+    public void UnLockRegular()
+    {
+        //100f - полный прогресс
+        Social.ReportProgress(GPGSIds.achievement_lava_boss, 100f, null);
     }
 }
